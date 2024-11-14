609bec3f48f2413d50959583fbd4c8415d29c834
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-14 08:15:08 +0200

    rip

--- react-portfolio-master/src/content_option.js
+++ react-portfolio-master/src/content_option.js
@@ -1,18 +1,18 @@
-const logotext = "JOHN";
+const logotext = "SRB";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Sannijs Bondars",
+    description: "Student and currently am learning or atleast im trying",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "Sannijs R Bondars",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love it",
+        second: "I create something fun ",
+        third: "I develop better life",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "https://github.com/sannijs/img/blob/main/Mask%20group.png?raw=true",
 };
 
 const dataabout = {
