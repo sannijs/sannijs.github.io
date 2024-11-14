49f7c6da1bf2c384c5c27dc4818fdff65449658c
Sanvirus <sannijs.bondars123@gmail.com>
2024-11-14 02:47:07 +0200

    stufffff

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,60 +1,36 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Grid, Container, Button, Box } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Home from './components/Home';
+import About from './components/About.js';
+import Projects from './components/Projects.js/index.js';
+import Contact from './components/Contact';
 
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#000',
+    },
+  },
+});
 
 function App() {
-    return (
-        <div>
-          <Button variant="contained" color="primary" sx={{ mt: 2, px: 4 }}>
-        Learn More
-          </Button>
-            {/* AppBar for Navigation */}
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        My Portfolio
-                    </Typography>
-                    <Button color="inherit">About</Button>
-                    <Button color="inherit">Projects</Button>
-                    <Button color="inherit">Contact</Button>
-                </Toolbar>
-            </AppBar>
-
-            {/* Main Content */}
-            <Container>
-                <Box mt={4}>
-                    <Typography variant="h1" align="center" gutterBottom>
-                        Welcome to My Portfolio
-                    </Typography>
-                    <Typography variant="body1" align="center">
-                        A showcase of my projects and skills.
-                    </Typography>
-                </Box>
-
-                {/* Project Section */}
-                <Grid container spacing={4} mt={4}>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Box bgcolor="primary.main" p={2} color="white" textAlign="center">
-                            <Typography variant="h5">Project 1</Typography>
-                            <Typography>Project description goes here.</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Box bgcolor="secondary.main" p={2} color="white" textAlign="center">
-                            <Typography variant="h5">Project 2</Typography>
-                            <Typography>Project description goes here.</Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <Box bgcolor="primary.main" p={2} color="white" textAlign="center">
-                            <Typography variant="h5">Project 3</Typography>
-                            <Typography>Project description goes here.</Typography>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>
-    );
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
